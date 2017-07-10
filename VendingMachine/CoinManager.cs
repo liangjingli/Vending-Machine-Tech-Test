d65a5c63c2778530b48dfce5b82e0e73d4e4d37e
slaiman ahmad <sulaimanahmad321@gmail.com>
2017-07-10 13:24:28 +0100

    Adds CoinMangager class, Passes tests for Coinmanager containing a list for user coins

---
+++ VendingMachine/CoinManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    public class CoinManager
+    {
+		private List<Coin> _userCoins = new List<Coin>();
+
+		public List<Coin> UserCoins
+		{
+			get { return _userCoins; }
+			set { _userCoins = value; }
+		}
+
+		public CoinManager()
+		{
+			this.UserCoins = UserCoins;
+		}
+    }
+}
---
+++ VendingMachineTest/CoinManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using VendingMachine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace VendingMachineTest
+{
+	[TestClass]
+	public class CoinManagerTest
+	{
+		[TestMethod]
+		public void HasListForUserCoins()
+		{
+			CoinManager coinManager = new CoinManager();
+			Assert.IsInstanceOfType(coinManager.UserCoins, typeof(List<Coin>));
+		}
+	}
+}
