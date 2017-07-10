d207749410167d2aacfb74aee79ceea8f051b9a9
slaiman ahmad <sulaimanahmad321@gmail.com>
2017-07-10 04:35:40 +0100

    Add Unit Tests folder to project

---
+++ VendingMachineTest/ItemTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTest
+{
+    [TestClass]
+    public class ItemTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
