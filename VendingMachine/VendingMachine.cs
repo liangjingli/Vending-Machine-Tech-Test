ba878b949eba2d04a6a20b15f51e9458bcce2867
slaiman ahmad <sulaimanahmad321@gmail.com>
2017-07-11 17:37:48 +0100

    Adds VendingMachine Class, passes first test for ItemStock property

---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachineProject
+{
+
+    public class VendingMachine
+    {
+		public static List<Item> itemStock = new List<Item> { new Item("water", 60),
+															  new Item("water", 60),
+															  new Item("crisps", 40),
+															  new Item("crisps", 40)
+		};
+
+		public List<Item> ItemStock { get; private set; }
+		public CoinManager CoinManger { get; set; }
+
+		public VendingMachine()
+		{
+			ItemStock = itemStock;
+			CoinManger = new CoinManager();
+		}
+	}
+}
---
+++ VendingMachineTest/VendingMacineTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using VendingMachineProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace VendingMachineProjectTest
+{
+	[TestClass]
+	public class VendingMachineTest
+	{
+		[TestMethod]
+		public void HasItemStock()
+		{
+			VendingMachine vendingMachine = new VendingMachine();
+			Assert.IsInstanceOfType(vendingMachine.ItemStock, typeof(List<Item>));
+		}
+
+	}
+}
