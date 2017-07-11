9b7a973569b97a57fad50be7764b284088e1979d
slaiman ahmad <sulaimanahmad321@gmail.com>
2017-07-11 14:18:13 +0100

    Initializes coinStock list outside of CoinManager Constructor, changes setteer of UserCoins and CoinsToReturn to private.

--- VendingMachine/CoinManager.cs
+++ VendingMachine/CoinManager.cs
@@ -8,19 +8,18 @@ namespace VendingMachineProject
     public class CoinManager
     {
 		public static Coin[] ValidUserCoins = { new Coin(50) };
-
-		public List<Coin> UserCoins { get; set; }
-		public List<Coin> CoinsToReturn { get; set; }
+		public static List<Coin> coinStock = new List<Coin>{ new Coin(20),
+															 new Coin(10),
+															 new Coin(10),
+															 new Coin(20),
+															 new Coin(20)
+		};
+		public List<Coin> UserCoins { get; private set; }
+		public List<Coin> CoinsToReturn { get; private set; }
 		public List<Coin> CoinStock { get; set; }
 
 		public CoinManager()
 		{
-			List<Coin> coinStock = new List<Coin> { new Coin(20),
-													new Coin(10),
-													new Coin(10),
-													new Coin(20),
-													new Coin(20)
-			};
 			CoinStock = coinStock;
 			UserCoins = new List<Coin>();
 			CoinsToReturn = new List<Coin>();
