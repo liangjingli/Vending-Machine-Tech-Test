d0b341470930075ff561c3449a4da32e87b9c8ec
slaiman ahmad <sulaimanahmad321@gmail.com>
2017-07-12 00:43:54 +0100

    Add full integration test for VendingMachine

--- VendingMachineTest/IntegrationTest.cs
+++ VendingMachineTest/IntegrationTest.cs
@@ -27,5 +27,28 @@ namespace VendingMachineProjectTest
 			Assert.IsTrue(UserCoins.Contains(FiftyPence));
 		}
 
+		[TestMethod]
+		public void FullInegrationTest()
+		{
+			VendingMachine vendingMachine = new VendingMachine();
+			List<Coin> UserCoins = vendingMachine.VendorCoinManger.UserCoins;
+			List<Item> ItemList = vendingMachine.Selection;
+			vendingMachine.DisplayItems();
+			// Will output items in vending machine to console
+			vendingMachine.Select("water");
+			vendingMachine.Select("water");
+			// Adds selections to your selection list
+			vendingMachine.Insert(new Coin(50));
+			// Prompts for right amount of coins to be inserted for total of your selection
+			vendingMachine.Insert(new Coin(50));
+			vendingMachine.Insert(new Coin(50));
+			// Transaction will end and user will be shown their items and correct change given
+
+			Assert.IsTrue(UserCoins.Count ==  0);
+			// Once transaction is done user coins will be reset
+			Assert.IsTrue(ItemList.Count == 0);
+			// Will also reset user Selection list
+		}
+
 	}
 }
